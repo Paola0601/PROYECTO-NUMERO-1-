8090806ab1f845e53837acfcd18cd2b90eb51433
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-03-05 11:11:24 -0500

    primer commid de bienvenida

---
+++ bienvenida.java
@@ -0,0 +1,9 @@
+public class Bienvenida
+{
+    public static void main(String args[]) {
+    
+        /** */
+        System.out.println ("¡Bienvenido al curso de programacion web!");
+
+    }
+}
--- ejercicio1.java
+++ ejercicio1.java
@@ -3,7 +3,7 @@ public class HolaMundo
     public static void main(String args[]) {
     
         
-        System.out.println ("¡Hola mundo!");
+        System.out.println ("Bienvenido al curso de programacion web 2 ");
 
     }
 }
